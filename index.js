73715b91abaea5cf0063d0a11975eaa682b5935a
DevShishodia <90478486+DevShishodia@users.noreply.github.com>
2022-01-16 07:05:22 -0800

    Weather App

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-// Declaring the variables
-let lon;
-let lat;
 let temperature = document.querySelector(".temp");
 let summary = document.querySelector(".summary");
 let loc = document.querySelector(".location");
 let icon = document.querySelector(".icon");
-const kelvin = 273;
-
-
-
-	// API ID
-	// const api = "6d055e39ee237af35ca066f35474e9df";
-    const ACCES_KEY = "09cba7b0c8244664b97122257221601";
+const ACCES_KEY = "09cba7b0c8244664b97122257221601";
 
 
 
@@ -20,10 +11,9 @@ const kelvin = 273;
 		return response.json();
 		})
 		.then((data) => {
-		console.log(data);
 		temperature.textContent = data.current.temp_c  + "°C";
 		summary.textContent = data.condition;
-		loc.textContent = data.location.name + "," + data.location.region+',   '+data.location.country;
+		loc.textContent = data.location.name + "," + data.location.region+','+data.location.country;
 		let icon1 = data.current.condition.icon;
 		icon.innerHTML =
 			`<img src="${icon1}" style= 'height:10rem'/>`;
